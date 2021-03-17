140f2e12c0bc17a901e287b11c9d419fd2a2c692
James Smith <smith12james12@gmail.com>
2021-03-16 18:22:04 -0600

    updated cePet assignment

--- a03/src/a03/Autocomplete.java
+++ a03/src/a03/Autocomplete.java
@@ -30,14 +30,19 @@ public final class Autocomplete {
     // Return all terms that start with the given prefix, in descending order of weight.
     // if no matches return an array of size 0
     public Term[] allMatches(String prefix) {
+        Term[] temp1;
         if(prefix == null) throw new java.lang.NullPointerException();
 
         int first1 = BinarySearchDeluxe.firstIndexOf(terms, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
-        if(first1 == -1) return new Term[0];
+        if(first1 == - 1) { return new Term[0]; }
 
         int last1 = BinarySearchDeluxe.lastIndexOf(terms, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
 
-        Term[] temp1 = new Term[1+last1-first1];
+        if((last1 - first1) + 1 == 2) {
+            temp1 = new Term[1];
+        } else {
+            temp1 = new Term[1 + last1 - first1];
+        }
 
         for(int i = 0; i<temp1.length; i++){
             temp1[i] = terms[first1++];
@@ -52,42 +57,22 @@ public final class Autocomplete {
         if(prefix.equals(null)) { throw new NullPointerException(); }
 
         Term searchTerm = new Term(prefix, 0);
-
         int first1 = BinarySearchDeluxe.firstIndexOf(terms, searchTerm,Term.byPrefixOrder(prefix.length()));
         int last1 = BinarySearchDeluxe.lastIndexOf(terms, searchTerm,Term.byPrefixOrder(prefix.length()));
         int found =  1 + (last1 - first1);
 
-        if(first1 <= 0) { return 0; }
-        if(found <= 0) { return 0; }
-
-        return found;
+        if(first1 < 0) { return 0; }
+        else if(found == 2) { return 1; }
+        else if(found <= 0) { return 0; }
+        else if(last1 == first1) { return 1; }
+        else { return found; }
 
     }
 
     public static void main(String[] args) {
-//        String filename = "src/a03/cities.txt";
-//
-//        In in = new In(filename);
-//        int num = in.readInt();
-//        Term[] terms = new Term[num];
-//
-//        for (int i = 0; i < num; i++) {
-//            double weight = in.readDouble();
-//            in.readChar();
-//            String query = in.readString();
-//            terms[i] = new Term(query, weight);
-//        }
-//
-//        int k = 10;
-//        Autocomplete autocomplete = new Autocomplete(terms);
-//        while (StdIn.hasNextLine()) {
-//            String prefix = StdIn.readLine();
-//            Term[] results = autocomplete.allMatches(prefix);
-//
-//            for (int i = 0; i < Math.min(k, results.length); i++) {
-//                StdOut.println("results" + results[i]);
-//            }
-//        }
+
+
+
     }
 
 }
