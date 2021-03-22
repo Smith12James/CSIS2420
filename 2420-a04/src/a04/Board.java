9c22c947520df4dcd739608fb6edf7061126ea63
Smith12James <smith12james12@gmail.com>
2021-03-22 00:05:53 -0600

    worked on bugs in a04

--- 2420-a04/src/a04/Board.java
+++ 2420-a04/src/a04/Board.java
@@ -1,25 +1,28 @@
 package a04;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Board{
 
     int size;
-    int[][] blocks;
+    int[][] board;
     int[][] solution;
 
     // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
-    public Board(int[][] blocks) {
-        size = blocks.length;
-        this.blocks = blocks;
+    public Board(int[][] a) {
+        this.size = a.length;
+        this.board = new int[size][size];
 
         //Copying the passed array to the new array
-        for(int i=0; i<blocks.length; i++) {
-            for(int j=0; j<blocks[i].length; j++) {
-                this.blocks[i][j]=blocks[i][j];
+        for(int i=0; i<size; i++) {
+            for(int j=0; j< size; j++) {
+                this.board[i][j]=a[i][j];
             }
         }
         solutionBoard();
@@ -36,12 +39,12 @@ public class Board{
          * {4,5,6}
          * {7,8,0}
          */
-
+        solution = new int[size][size];
         int count = 1;
-        for (int i = 0; i < solution.length; i++) {
-            for (int j = 0; j < solution[i].length; j++) {
-                if(count == size*size) solution[i][j] = 0; // assign last position as 0
-                else solution[i][j] = count; // set board position equal to index
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(count == size*size) solution[i][j] = 0;
+                else solution[i][j] = count;
                 count++;
             }
         }
@@ -56,11 +59,16 @@ public class Board{
     public int hamming() {
 
         int outOfPlace = 0; //number of slots that are incorrect
+        int number; //the number to be compared
+        int row, col;
 
         //for loop to iterate through the array to find which slots are incorrect
-        for (int i = 0; i < blocks.length; i++) {
-            for (int j = 0; j < blocks.length; j++) {
-                if(solution[i][j] != blocks[i][j]) outOfPlace++;
+        for (int i = 1; i < size*size; i++) {
+            row = i / size;
+            col = i % size;
+            number = board[row][col];
+            if(number != i + 1) {
+                outOfPlace++;
             }
         }
         return outOfPlace;
@@ -68,12 +76,51 @@ public class Board{
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan()  {
-        return hamming() + size(); //THIS COULD BE WRONG AND PROBABLY IS WRONG :)
+        int distance = 0; // this will be the sums of all disances
+        int row1, col1; //row and col of mixed board
+        int row2 = 0, col2 = 0; //row and col of solution board;
+        int number; //number that we are comparing
+
+        /*
+         * n | / | %
+         * 0   0   0
+         * 1   0   1
+         * 2   0   2
+         * 3   1   0
+         * 4   1   1
+         * 5   2   2
+         * 6   2   0
+         */
+
+        for (int i = 0; i < size*size; i++) {
+            row1 = i / size;
+            col1 = i % size;
+            number = board[row1][col1];
+            if(number == 0) continue; // dont count the zero
+
+
+            for (int j = 0; j < size*size; j++) {
+                if(solution[j / size][j % size] == number) {
+                    row2 = j/size;
+                    col2 = j%size;
+                }
+            }
+            distance += Math.abs((row2-row1)) + Math.abs((col2-col1));
+        }
+        return distance;
     }
 
     // is this board the goal board?
     public boolean isGoal()  {
-        return (hamming()==0);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(board[i][j] != solution[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     // is this board solvable?
@@ -88,10 +135,10 @@ public class Board{
 
                 // Value 0 is used for empty space
                 //checks whether the number before is greater than the number after or an inversion
-                if (blocks[j][i] > 0 &&
-                        blocks[j][i] > blocks[i][j])
+                if (board[j][i] > 0 &&
+                        board[j][i] > board[i][j])
                     inv_count++;
-                if (blocks[i][j] == 0)
+                if (board[i][j] == 0)
                     zeroRow = i;
             }
         }
@@ -102,63 +149,58 @@ public class Board{
         return true;
     }
 
-    // does this board equal y? lmao this is all wrong
+    // does this board equal y?
     public boolean equals(Object y)     {
         Board yBoard = (Board) y;
         if (y == null || getClass() != y.getClass()
-                || size != yBoard.size || !Arrays.deepEquals(blocks, yBoard.blocks))
+                || size != yBoard.size || !Arrays.deepEquals(board, yBoard.board))
             return false;
-        if (this.blocks == y)
+        if (this.board == y)
             return true;
-        return (blocks.equals(y));
+        return (board.equals(y));
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors()  {
-        MinPQ<Board> queue = new MinPQ<Board>();
+        Stack<Board> queue = new Stack<Board>();
 
         //check if i+-1 and j+-1 are available, if so then we can exch()
         int zColumn = 0; //Location of the 0 column
         int zRow = 0; // location of the 0 row
 
-        //for loop to find where the zero is on the board
-        for (int i = 0; i < blocks.length; i++) {
-            for (int j = 0; j < blocks.length; j++) {
-                if(blocks[i][j] == 0) {
+        // for loop to find where the zero is on the board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if(board[i][j] == 0) {
                     zColumn = i;
                     zRow = j;
                 }
             }
         }
         //There can be only 2-4 moves a board. These loops should determin if an exchange can be done
-        //Also the if statements can be wrong in this i kinda fired from the hip
 
-        if(zColumn+1 != 3 ) queue.insert(exch(blocks, zColumn, zRow, zColumn + 1, zRow)); // Exchanges to the right
-        if(zColumn-1 != -1) queue.insert(exch(blocks, zColumn, zRow, zColumn - 1, zRow)); // Exchanges to the left
-        if(zRow+1 != 3) queue.insert(exch(blocks, zColumn, zRow, zColumn, zRow + 1)); // Exchanges up one
-        if(zRow-1 != -1) queue.insert(exch(blocks, zColumn, zRow, zColumn, zRow - 1)); // Exchanges down one
-
-        //This could possibly be another solution
-
-        //if(zColumn / size == ) I was going somewhere with this but kidna lost it
-        //if(zRow % size == )
+        if(zColumn + 1 != size ) queue.push(exch(createACopy(), zColumn, zRow, zColumn + 1, zRow)); // Exchanges to the right
+        if(zColumn != 0) queue.push(exch(createACopy(), zColumn, zRow, zColumn - 1, zRow)); // Exchanges to the left
+        if(zRow + 1 != size) queue.push(exch(createACopy(), zColumn, zRow, zColumn, zRow + 1)); // Exchanges up one
+        if(zRow != 0) queue.push(exch(createACopy(), zColumn, zRow, zColumn, zRow - 1)); // Exchanges down one
 
         return queue;
     }
 
-    public Board exch(int[][] a, int r1, int c1, int r2, int c2) {
-        int[][] temp = new int[size][size];
+    private int[][] createACopy() {
+        int[][] newBoard = this.board.clone();
+        return newBoard;
 
-        for(int i=0; i<a.length; i++) {
-            for(int j=0; j<a[i].length; j++) {
-                temp[i][j]=a[i][j];
-            }
-        }
+    }
 
-        temp[c1][r1] = a[c2][r2];
+    private Board exch(int[][] a, int i, int j, int i2, int j2) {
+        int temp1 = this.board[i][j];
+        int temp2 = this.board[i2][j2];
 
+        a[i2][j2] = temp1;
+        a[i][j] = temp2;
 
-        return new Board(temp);
+        return new Board(a);
     }
 
     // string representation of this board (in the output format specified below)
@@ -167,15 +209,45 @@ public class Board{
         s.append(size + "\n");
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                s.append(String.format("%2d ", blocks[i][j]));
+                s.append(String.format("%2d ", board[i][j]));
             }
             s.append("\n");
         }
         return s.toString();
     }
 
+    private int[][] getGoal() {
+        return solution;
+    }
+
     // unit tests (not graded)
     public static void main(String[] args) {
-        
+        int[][] board1 = { { 0, 1, 2 }, { 4, 3, 5 }, { 7, 6, 8 } }; // 4
+        int[][] board2 = {{2, 1}, {0,3}};
+
+        Board myBoard = new Board(board1);
+        StdOut.println("----------");
+        StdOut.println("Board: ");
+        StdOut.println(myBoard);
+        StdOut.println("Neighbor: ");
+        StdOut.println(myBoard.neighbors());
+//        StdOut.println("Solution: ");
+//        StdOut.println(new Board(myBoard.getGoal()));
+//        StdOut.println("Manhattan Value: " + myBoard.manhattan());
+//        StdOut.println("Hamming Value: " + myBoard.hamming());
+//        StdOut.println();
+
+        Board myBoard2 = new Board(board2);
+        StdOut.println("----------");
+        StdOut.println("Board: ");
+        StdOut.println(myBoard2);
+        StdOut.println("Neighbor: ");
+        StdOut.println(myBoard2.neighbors());
+//        StdOut.println("Solution: ");
+//        StdOut.println(new Board(myBoard2.getGoal()));
+//        StdOut.println("Manhattan Value: " + myBoard2.manhattan());
+//        StdOut.println("Hamming Value: " + myBoard2.hamming());
+//        StdOut.println();
     }
+
 }
